4980e5edf4964def7815fef5896049231f0aea0d
Krittanut <sangsri.krittanut@gmail.com>
2021-08-07 17:06:04 +0700

    third commit

--- calmoney/src/app/app.module.ts
+++ calmoney/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { NbThemeModule, NbLayoutModule, NbInputModule, NbDatepickerModule, NbBut
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { NbIconModule } from '@nebular/theme';
 import { CalComponent } from './cal/cal.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+// import { ChartsModule, WavesModule } from 'angular-bootstrap-md'
+
 
 @NgModule({
   declarations: [
@@ -25,6 +28,7 @@ import { CalComponent } from './cal/cal.component';
     NbInputModule,
     NbDatepickerModule.forRoot(),
     NbButtonModule,
+    MDBBootstrapModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
